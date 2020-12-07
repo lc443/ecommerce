1843096a2afabbaa6e08cdb80f62c18cfaf0b067
Leron Charles <LeroniMac@Lerons-iMac.fios-router.home>
2020-12-07 10:57:08 -0500

    started backend implementation

---
+++ ecommerce-backend/src/main/java/com/leron/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.leron.dao;
+
+import com.leron.models.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel ="productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
